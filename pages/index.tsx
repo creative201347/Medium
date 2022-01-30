a32f9cd4cdd4b047a1e372f65451ff3501092c10
creative201347 <creative201347@gmail.com>
2022-01-30 17:21:08 +0545

    retreviewing data from sanity

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import Header from '../components/Header'
+import { sanityClient, urlFor } from '../sanity'
+import { Post } from '../typing'
 
-export default function Home() {
+interface Props {
+  posts: [Post]
+}
+
+export default function Home({ posts }: Props) {
+  console.log(posts)
   return (
-    <div className="max-w-7xl mx-auto">
+    <div className="mx-auto max-w-7xl">
       <Head>
         <title>Medium</title>
         <link rel="icon" href="/favicon.ico" />
@@ -11,24 +19,82 @@ export default function Home() {
 
       <Header />
 
-      <div className='flex justify-between items-center bg-yellow-400
-      border-y border-black py-10 lg:py-0'>
-        <div className='px-10 space-y-5'>
-          <h1 className='text-6xl max-w-xl font-serif'>
-            <span className='underline decoration-black decoration-4 
-            '>Medium</span> is a place to write, read and connect</h1>
+      <div
+        className="flex items-center justify-between border-y
+      border-black bg-yellow-400 py-10 lg:py-0"
+      >
+        <div className="space-y-5 px-10">
+          <h1 className="max-w-xl font-serif text-6xl">
+            <span
+              className="underline decoration-black decoration-4 
+            "
+            >
+              Medium
+            </span>
+            is a place to write, read and connect
+          </h1>
           <h2>
-            It's easy and free to post your thinking on any topic
-            and connect with millions of readers
+            It's easy and free to post your thinking on any topic and connect
+            with millions of readers
           </h2>
         </div>
-        
-        <img className='hidden md:inline-flex h-48 lg:h-auto'
-        src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" alt="" />
+
+        <img
+          className="hidden h-48 md:inline-flex lg:h-auto"
+          src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
+          alt=""
+        />
       </div>
 
-
-
+      <div className="grid grid-cols-1 gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols-3">
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className="group cursor-pointer overflow-hidden rounded-lg border">
+              <img
+                className="h-60 w-full object-cover transition-transform
+              duration-200 ease-in-out group-hover:scale-105"
+                src={urlFor(post.mainImage).url()!}
+                alt=""
+              />
+              <div className="flex justify-between bg-white p-5">
+                <div>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-xs">
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+                <img
+                  className="h-12 w-12 rounded-full"
+                  src={urlFor(post.author.image).url()!}
+                  alt=""
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"]{
+    _id,
+    title,
+    author ->{
+    name,
+    image
+  }, 
+  description,
+  mainImage,
+  slug
+  }`
+
+  const posts = await sanityClient.fetch(query)
+
+  return {
+    props: {
+      posts,
+    },
+  }
 }
